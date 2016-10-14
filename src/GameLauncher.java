f266adbfb0a111d9c1a625b7d36dd283a00f1848
m0rn <morn88@gmail.com>
2016-10-14 19:39:39 +0300

    Первая стадия готовности игры

--- src/GameLauncher.java
+++ src/GameLauncher.java
@@ -2,4 +2,8 @@
  * Created by ЭМПРиО on 14.10.2016.
  */
 public class GameLauncher {
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -2,4 +2,9 @@
  * Created by ЭМПРиО on 14.10.2016.
  */
 public class Player {
+    int number = 0;
+    public void guess(){
+        number = (int) (Math.random() * 10);
+        System.out.println("Думаю, это число " + number);
+    }
 }
